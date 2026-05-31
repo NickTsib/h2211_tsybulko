11697166e0061b4fae6b7d90c950ba36ca0c0793
Serhii Horchynskyi <tnasergey@gmail.com>
2026-05-31 13:53:57 +0300

    para9

---
+++ pythonProject2/para9p3.py
@@ -0,0 +1,15 @@
+from bs4 import BeautifulSoup
+import requests
+
+index = int(input("Введіть номер в списку: "))
+
+response = requests.get("https://coinmarketcap.com/")
+if response.status_code == 200:
+    soup = BeautifulSoup(response.text, features="html.parser")
+    soup_list = soup.find_all("div", {"class": "sc-631098c-0 ilZTOW"})
+    soup_list1 = soup.find_all("p", {"class": "sc-c1554bc0-0 eWrlhi coin-item-name"})
+
+    res1 = soup_list1[index].get_text()
+    res = soup_list[index].find("span")
+
+    print(f"Валюта:{res1} Вартість:{res.text}")
